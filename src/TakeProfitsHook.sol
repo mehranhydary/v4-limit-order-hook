73adcb85a5b0dde7e5f8a8f5e645c2060e12e0a4
mehranhydary <mehranhydary@users.noreply.github.com>
2024-08-01 11:52:16 -0400

    Added simple TakeProfitsHook.sol

---
+++ src/TakeProfitsHook.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+
+import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
+
+contract TakeProfitsHook is BaseHook, ERC1155 {
+    // StateLibrary is new here and we haven't seen that before
+    // It's used to add helper functions to the PoolManager to read
+    // storage values.
+    // In this case, we use it for accessing `currentTick` values
+    // from the pool manager
+    using StateLibrary for IPoolManager;
+
+    // PoolIdLibrary used to convert PoolKeys to IDs
+    using PoolIdLibrary for PoolKey;
+    // Used to represent Currency types and helper functions like `.isNative()`
+    using CurrencyLibrary for Currency;
+    // Used for helpful math operations like `mulDiv`
+    using FixedPointMathLib for uint256;
+
+    // Errors
+    error InvalidOrder();
+    error NothingToClaim();
+    error NotEnoughToClaim();
+
+    // Constructor
+    constructor(
+        IPoolManager _manager,
+        string memory _uri
+    ) BaseHook(_manager) ERC1155(_uri) {}
+
+    // BaseHook Functions
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: true,
+                beforeAddLiquidity: false,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    function afterInitialize(
+        address,
+        PoolKey calldata key,
+        uint160,
+        int24 tick,
+        bytes calldata
+    ) external override onlyByPoolManager returns (bytes4) {
+        // TODO
+        return this.afterInitialize.selector;
+    }
+
+    function afterSwap(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        BalanceDelta,
+        bytes calldata
+    ) external override onlyByPoolManager returns (bytes4, int128) {
+        // TODO
+        return (this.afterSwap.selector, 0);
+    }
+}
