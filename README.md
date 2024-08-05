6b71a6d466c616ff71bf3890a68e34d0eb8d5cc1
mehranhydary <mehranhydary@users.noreply.github.com>
2024-08-05 11:52:36 -0400

    Placing and cancellling orders work

--- README.md
+++ README.md
@@ -1,5 +1,46 @@
 ## Liquidity operator for limit orders (via Uniswap V4 Hook)
 
+## Mechanism Design
+
+1. Place an order
+2. Cancel an order (if it hasn't been filled yet)
+3. Ability to withdraw / redeem output tokens after executing an order
+
+None of these things are tied to the core Uniswap codebase. These will be public functions on the hook contract that anyone can call directly.
+
+Once completed (order placed):
+
+1. When price is right, how do we execute it? How do we do this as a part of a hook?
+2. How do we know that the price is right? (When to execute)
+3. How do we send / let the user redeem output from order
+
+Assume there's a pool of tokens a and b. We will treat A as token 0, B as token 1. Assume tick is 500, tick means A is more valuable than B.
+
+Types of TP orders that can be placed:
+
+1. Sell amount of A as A gets more valuable than B
+2. Sell some amount of B as B gets more valuable (A drops in value)
+
+Case 1: Tick goes up further, beyond what it is right now (500)
+Case 2: Tick goes up, below what is right now (500)
+
+When do these tick values change? When trades take place
+
+1. Alice places order to sell A for B when Tick = 600
+2. Bob comes around and does a swap on the pool to buy A for B, increases the tick. Let's say after Bob's swap, new tick = 700.
+3. Inside the `afterSwap` hook function, we can see this happening -> Tick just shifted from 500 to 700 because of Bob's swap.
+4. Check if we have TP orders placed in the opposite direction in the rage that the tick was shifted. See Alice's order there.
+5. We can execute Alice's order now that her requirements have been met.
+
+## Assumptions
+
+1. We are not concerned with gas costs / limits
+   1.1. Bob will pay gas for Alice's order in the above example.
+   1.2. We are not gonna limit how many orders we execute because of a price shift
+2. We will ignore slippage requirements for placed limit orders
+   2.1. When Alice places her order, ideally - she should also set some sort of slippages (sort of min token B to get back)
+3. We are not gonna support pools that have native ETH . token as one of the currencies. We only support ERC-20:ERC20 pools.
+
 ## Usage
 
 ### Build
--- src/TakeProfitsHook.sol
+++ src/TakeProfitsHook.sol
@@ -19,6 +19,19 @@ import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 
 import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
 
+/**
+  1. Placing orders
+  We need:
+  - pool she's placing order, poolKey
+  - which token to sell (eth or usdc)
+  - what tick she wants to sell at 
+  - how many toens she wants to sell
+
+  -- 
+  ticks are integers that are at various values separated by some tick spacing
+ */
+
+// NOTE: This could've been ERC-6909, but we're using ERC-1155 for simplicity
 contract TakeProfitsHook is BaseHook, ERC1155 {
     // StateLibrary is new here and we haven't seen that before
     // It's used to add helper functions to the PoolManager to read
@@ -33,6 +46,26 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
     using CurrencyLibrary for Currency;
     // Used for helpful math operations like `mulDiv`
     using FixedPointMathLib for uint256;
+
+    // We don't store information about who place the order
+    // this is intent ional, we will mint them our own token to represent an open position / order
+
+    // it is possible for multiple people to want to place the same order
+    // alice wants to sell 5 eth at 3500 usdc each
+    // bob wants to do 10 eth at the same price
+
+    // we will combine bob and alice order into single order, do it as a single swap
+
+    // Pool => tick to sell => direction of swap => amount of tokens to sell
+    mapping(PoolId poolId => mapping(int24 tickToSellAt => mapping(bool zeroForOne => uint256 inputAmount)))
+        public pendingOrders;
+
+    // Is for each order that exists, how many erc1155 tokens have been minted out
+    // equal to total amount of input amount tokens that exist at that order
+    mapping(uint256 orderIds => uint256 tokenSupply) public claimTokensSupply;
+
+    mapping(uint256 orderId => uint256 outputClaimable)
+        public claimableOutputTokens;
 
     // Errors
     error InvalidOrder();
@@ -91,5 +124,170 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
     ) external override onlyByPoolManager returns (bytes4, int128) {
         // TODO
         return (this.afterSwap.selector, 0);
+    }
+
+    // Write the redeem function
+    // Write the executeOrder function
+    // Write the swapAndSettleBalances function
+
+    function getLowerUsableTick(
+        int24 tick,
+        int24 tickSpacing
+    ) private pure returns (int24) {
+        int24 intervals = tick / tickSpacing;
+        // what happens if tick is negative
+        // alice provides tick = -75
+        if (tick < 0 && tick % tickSpacing != 0) {
+            intervals--;
+        }
+
+        return intervals * tickSpacing;
+    }
+
+    function getOrderId(
+        PoolKey calldata key,
+        int24 tick,
+        bool zeroForOne
+    ) public pure returns (uint256) {
+        return
+            uint256(keccak256(abi.encodePacked(key.toId(), tick, zeroForOne)));
+    }
+
+    function placeOrder(
+        PoolKey calldata key,
+        int24 tickToSellAt,
+        bool zeroForOne,
+        uint256 inputAmount
+    ) external returns (int24) {
+        int24 tick = getLowerUsableTick(tickToSellAt, key.tickSpacing);
+        pendingOrders[key.toId()][tick][zeroForOne] += inputAmount;
+        uint256 orderId = getOrderId(key, tick, zeroForOne);
+        claimTokensSupply[orderId] += inputAmount;
+        _mint(msg.sender, orderId, inputAmount, "");
+
+        address sellToken = zeroForOne
+            ? Currency.unwrap(key.currency0)
+            : Currency.unwrap(key.currency1);
+        IERC20(sellToken).transferFrom(msg.sender, address(this), inputAmount);
+
+        return tick;
+    }
+
+    // TODO: Handle partial cancellation (add another field, compare and burn partially)
+    function cancelOrder(
+        PoolKey calldata key,
+        int24 tickToSellAt,
+        bool zeroForOne
+    ) external {
+        int24 tick = getLowerUsableTick(tickToSellAt, key.tickSpacing);
+        uint256 orderId = getOrderId(key, tick, zeroForOne);
+        uint256 orderTokens = balanceOf(msg.sender, orderId);
+        if (orderTokens == 0) {
+            revert InvalidOrder();
+        }
+        pendingOrders[key.toId()][tick][zeroForOne] -= orderTokens;
+        claimTokensSupply[orderId] -= orderTokens;
+        _burn(msg.sender, orderId, orderTokens);
+
+        Currency token = zeroForOne ? key.currency0 : key.currency1;
+        token.transfer(msg.sender, orderTokens);
+    }
+
+    function redeem(
+        PoolKey calldata key,
+        int24 tickToSellAt,
+        bool zeroForOne,
+        uint256 inputAmountToClaimFor
+    ) external {
+        int24 tick = getLowerUsableTick(tickToSellAt, key.tickSpacing);
+        uint256 orderId = getOrderId(key, tick, zeroForOne);
+
+        if (claimTokensSupply[orderId] == 0) {
+            revert NothingToClaim();
+        }
+
+        uint256 orderTokens = balanceOf(msg.sender, orderId);
+        if (orderTokens < inputAmountToClaimFor) {
+            revert NotEnoughToClaim();
+        }
+
+        uint256 totalClaimableForOrder = claimableOutputTokens[orderId];
+        uint256 totalInputAmountForPosition = claimTokensSupply[orderId];
+
+        uint256 outputAmount = inputAmountToClaimFor.mulDivDown(
+            totalClaimableForOrder,
+            totalInputAmountForPosition
+        );
+
+        claimableOutputTokens[orderId] -= outputAmount;
+        claimTokensSupply[orderId] -= inputAmountToClaimFor;
+        _burn(msg.sender, orderId, inputAmountToClaimFor);
+
+        Currency token = zeroForOne ? key.currency1 : key.currency0;
+        token.transfer(msg.sender, outputAmount);
+    }
+
+    function swapAndSettleBalances(
+        PoolKey calldata key,
+        IPoolManager.SwapParams memory params
+    ) internal returns (BalanceDelta) {
+        BalanceDelta delta = poolManager.swap(key, params, "");
+
+        if (params.zeroForOne) {
+            if (delta.amount0() < 0) {
+                _settle(key.currency0, uint128(-delta.amount0()));
+            }
+
+            if (delta.amount1() > 0) {
+                _take(key.currency1, uint128(delta.amount1()));
+            }
+        } else {
+            if (delta.amount1() < 0) {
+                _settle(key.currency1, uint128(-delta.amount1()));
+            }
+
+            if (delta.amount0() > 0) {
+                _take(key.currency0, uint128(delta.amount0()));
+            }
+        }
+
+        return delta;
+    }
+
+    function executeOrder(
+        PoolKey calldata key,
+        int24 tick,
+        bool zeroForOne,
+        uint256 inputAmount
+    ) internal {
+        BalanceDelta delta = swapAndSettleBalances(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: zeroForOne,
+                // NOTE: Negative value provided to signify an "exact input for output" swap
+                amountSpecified: -int256(inputAmount),
+                sqrtPriceLimitX96: zeroForOne
+                    ? TickMath.MIN_SQRT_PRICE + 1
+                    : TickMath.MAX_SQRT_PRICE - 1
+            })
+        );
+
+        pendingOrders[key.toId()][tick][zeroForOne] -= inputAmount;
+        uint256 orderId = getOrderId(key, tick, zeroForOne);
+        uint256 outputAmount = zeroForOne
+            ? uint256(int256(delta.amount1()))
+            : uint256(int256(delta.amount0()));
+
+        claimableOutputTokens[orderId] += outputAmount;
+    }
+
+    function _settle(Currency currency, uint128 amount) internal {
+        poolManager.sync(currency);
+        currency.transfer(address(poolManager), amount);
+        poolManager.settle();
+    }
+
+    function _take(Currency currency, uint128 amount) internal {
+        poolManager.take(currency, address(this), amount);
     }
 }
